5beef4a58adcc44387880c628d09d06e589353ee
Jan Siekierski <jan.siekierski@ah.nl>
2023-11-27 02:31:46 +0100

    running the llm

--- conversational-service/run_llm.py
+++ conversational-service/run_llm.py
@@ -8,7 +8,7 @@ chroma = Chroma(persist_directory="../chroma_db", collection_name="youtube_trans
                 )
 
 # Load model and tokenizer
-model_name = "monology/openinstruct-mistral-7b"
+model_name = "ericzzz/falcon-rw-1b-instruct-openorca"
 model = AutoModelForCausalLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
@@ -16,19 +16,22 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 def ask_question(question):
     # Perform retrieval from Chroma
     similar_documents = chroma.similarity_search(question, top_k=3)
-    print("Retrieved context: " + str(similar_documents[0]))
+    print("Retrieved context: " + " \n".join(
+        [doc.page_content for doc in similar_documents]))
 
     # Combine retrieved context with the user query
-    combined_input = question + " " + " ".join([doc['text'] for doc in similar_documents])
+    combined_input = "[INST]You'll get a question and context. Base your answer only on the context provided. \n" \
+                     " Context: " + " ".join(
+    [doc.page_content for doc in similar_documents]) + "\n Question: " + question + "[/INST]"
 
     # Encode the input and generate a response
     inputs = tokenizer.encode(combined_input, return_tensors="pt")
-    outputs = model.generate(inputs, max_length=200)
+    outputs = model.generate(inputs, max_length=1000)
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
     return response
 
 
 # Example usage
-user_query = "What is artificial general intelligence?"
+user_query = "What is synthetic data and how can we create it?"
 answer = ask_question(user_query)
 print(answer)

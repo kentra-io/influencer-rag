8607a972d14c8016d910965851b1ee273898e23d
Krzysztof Golebiowski <krzysiek.golebiowski@gmail.com>
2024-01-04 00:54:26 +0100

    Created console_utils.py

---
+++ utils/console_utils.py
@@ -0,0 +1,7 @@
+def red(text): return f"\033[31m{text}\033[0m"
+def green(text): return f"\033[32m{text}\033[0m"
+def yellow(text): return f"\033[33m{text}\033[0m"
+def blue(text): return f"\033[34m{text}\033[0m"
+def magenta(text): return f"\033[35m{text}\033[0m"
+def cyan(text): return f"\033[36m{text}\033[0m"
+def bold(text): return f"\033[1m{text}\033[0m"

3076ec150efbbf27abd15aa8fd84f0a2fddf20b7
Krzysztof Golebiowski <krzysiek.golebiowski@gmail.com>
2024-01-04 00:57:41 +0100

    Implemented new data ingestion workflow

--- _2_create_embeddings.py
+++ _2_create_embeddings.py
@@ -1,10 +1,13 @@
 import json
 import logging
 import os
-
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+import time
+from datetime import timedelta
 
 import config
+from retrieval.punctuator import punctuate
+from retrieval.tiler import get_sentences, create_tiles
+from utils.console_utils import bold
 from vector_db import chroma_provider
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -16,43 +19,64 @@ chroma = chroma_provider.get_chroma()
 def process_transcript(file_path):
     logger.info(f"Processing file: {file_path}")
 
-    splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=128)
-
     with open(file_path, 'r') as file:
         data_list = json.load(file)
 
         for data in data_list:
-            chunks = splitter.split_text(data['transcript'])
+            try:
+                logger.info(f"Processing '{data['title']}'")
 
-            # for doc_number, chunk in enumerate(chunks):
-            #     print(f"- {doc_number}: '{chunk}' ({len(chunk.split())})")
-            # break
+                tiling_start_time = time.time()
 
-            video_metadata = {
-                "title": data['title'],
-                "channel": data['channel'],
-                "url": data['url'],
-                "istitle": False,
-                "file": file_path
-            }
+                punctuated_transcript = punctuate(data['transcript']).replace('\n', ' ')
 
-            video_title_metadata = video_metadata.copy()
-            video_title_metadata['istitle'] = True
+                transcript_sentences = get_sentences(punctuated_transcript)
 
-            # First chunk is the title, others follow
-            texts = [data['title']] + chunks
-            metadatas=[video_title_metadata] + [video_metadata] * (len(chunks))
+                tiles = create_tiles(transcript_sentences)
 
-            if len(config.channels) == 1:
-                # Adds IDs, might be used to obtain related chunks;
-                # temporarily works only with one channel defined in config.py
-                ids=[str(0)] + list(map(lambda x: str(x), list(range(1, len(chunks) + 1))))
-                chroma.add_texts(texts=texts, metadatas=metadatas, ids=ids)
-            else:
-                chroma.add_texts(texts=texts, metadatas=metadatas)
+                # TODO Move to a separate method or update create_tiles method
+                paragraphs = []
+                current_par = ""
+                for i, sentence in enumerate(transcript_sentences):
+                    current_par += " " + sentence
 
-            chroma.persist()
-            print(f"Added {len(chunks)} embeddings for video '{data['title']}'")
+                    if i + 1 in tiles[1:-1]:
+                        paragraphs.append(current_par)
+                        current_par = ""
+
+                paragraphs.append(current_par)
+                # End
+
+                tiling_time = time.time() - tiling_start_time
+
+                logger.info(
+                    f"Created {bold(len(paragraphs))} paragraphs in {bold(str(timedelta(seconds=tiling_time)))}")
+
+                video_metadata = {
+                    "title": data['title'],
+                    "channel": data['channel'],
+                    "url": data['url'],
+                    "file": file_path
+                }
+
+                for paragraph in paragraphs:
+                    chunk_metadata = video_metadata.copy()
+                    # TODO That is super not optimal and temporary solution, we should keep
+                    #  paragraphs and chunks in a separate database and use IDs instead
+                    #  (https://stackoverflow.com/questions/77030151/generating-and-using-chromadb-ids)
+                    chunk_metadata["paragraph"] = paragraph
+
+                    chunks = get_sentences(paragraph)
+
+                    metadata = [chunk_metadata] * (len(chunks))
+
+                    chroma.add_texts(texts=chunks, metadatas=metadata)
+
+                chroma.persist()
+
+            except Exception as e:
+                logger.error(f"Unknown exception has been raised, skipping the transcript: {e}", exc_info=True)
+
 
 def main():
     for channel in config.channels:

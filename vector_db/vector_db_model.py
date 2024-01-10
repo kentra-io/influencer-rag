b61dfe96ec09f03134c22e5acb9b1c497f053998
Krzysztof Golebiowski <krzysiek.golebiowski@gmail.com>
2024-01-10 11:18:06 +0100

    Normalized result scoring between Chroma and ES with a dirty temporary solution

--- vector_db/vector_db_model.py
+++ vector_db/vector_db_model.py
@@ -1,11 +1,11 @@
 from abc import ABC
-from dataclasses import dataclass
 from enum import Enum
 
 from langchain.schema.vectorstore import VectorStore
 
 from vector_db.chroma_provider import get_chroma
 from vector_db.elasticsearch_provider import get_elasticsearch
+
 
 class VectorDbType(Enum):
     ELASTICSEARCH = "ElasticSearch"
@@ -24,10 +24,20 @@ class VectorDb(ABC):
         self.vector_store.add_texts(texts=texts, metadatas=metadatas, ids=ids)
 
     def similarity_search_with_score(self, users_query, k):
-        return self.vector_store.similarity_search_with_score(users_query, k)
+        results = self.vector_store.similarity_search_with_score(users_query, k)
+
+        # TODO That is a temporary solution!
+        #  It seems that ES returns cosine distance while Chroma returns cosine similarity, so for now
+        #  I just flip the ES scores. We need investigate in the ES documentation.
+        if self.vector_db_type == VectorDbType.ELASTICSEARCH:
+            normalized_results = [(document, 1 - score) for document, score in results]
+        else:
+            normalized_results = results
+
+        return normalized_results
 
     def persist(self):
-        if (self.vector_db_type == VectorDbType.CHROMA):
+        if self.vector_db_type == VectorDbType.CHROMA:
             self.vector_store.persist()
 
 

5c9789c41a7828c553791e48e9295c9404d74f81
Krzysztof Golebiowski <krzysiek.golebiowski@gmail.com>
2024-01-10 11:09:53 +0100

    Organized imports

--- vector_db/elasticsearch_provider.py
+++ vector_db/elasticsearch_provider.py
@@ -1,12 +1,9 @@
-from langchain_community.vectorstores import ElasticsearchStore
-
-from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.document_loaders import TextLoader
-
 from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain_community.vectorstores import ElasticsearchStore
 
 elasticsearch_embedding_model = "all-MiniLM-L6-v2"
 elasticsearch_index_name = "youtube_transcripts"
+
 
 def get_elasticsearch():
     elastic_vector_search = ElasticsearchStore(

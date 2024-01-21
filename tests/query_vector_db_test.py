a1cf5c4e73d4c3f59a4293a0c466b72e8a414720
Krzysztof Golebiowski <krzysiek.golebiowski@gmail.com>
2024-01-21 22:33:54 +0100

    Skipped test that runs action which expects user input

--- tests/query_vector_db_test.py
+++ tests/query_vector_db_test.py
@@ -1,6 +1,9 @@
+import pytest
+
 from app import query_vector_db
 
 
+@pytest.mark.skip(reason="Expects user-input")
 def test_query_vector_db():
     print("starting test")
     query_vector_db.main()

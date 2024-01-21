a365f113f0bdc8f0936319f6529fae1e8469dc6e
Krzysztof Golebiowski <krzysiek.golebiowski@gmail.com>
2024-01-21 19:01:33 +0100

    Use singleton-like llm_model getter, so it does not break when the module is imported (but not run)

--- app/_3_run_llm_llama_cpp.py
+++ app/_3_run_llm_llama_cpp.py
@@ -12,12 +12,22 @@ from app.model.rag_response import RagResponse
 from app.vector_db.vector_db_model import get_vector_db
 
 
+llm_model = None
+
+
 def init():
     os.putenv("TOKENIZERS_PARALLELISM", str(config.TOKENIZERS_PARALLELISM))
 
 
 init()
-llm_model = get_llm_model(config.model_name, config.local_models_path)
+
+
+def get_llm_model_local():
+    global llm_model
+    if llm_model is None:
+        llm_model = get_llm_model(config.model_name, config.local_models_path)
+
+    return llm_model
 
 
 def prepare_transcription_fragments(relevant_movie_chunks, max_score: float):
@@ -68,7 +78,7 @@ def ask_question(users_query, enable_vector_search, k=config.k, vector_db=config
                   "based on your knowledge.")
 
     llm_model_response = (
-        llm_model.get_model_response(system, users_query))
+        get_llm_model_local().get_model_response(system, users_query))
 
     llm_user_response = llm_model_response.llm_user_response
     llm_full_response = llm_model_response.llm_full_response
